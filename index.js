ae28b9a6b12e7a8a2085276339b4075f7db94cb2
lysgut <ltuninetticivarolo@escuelasproa.edu.ar>
2023-04-27 11:29:50 -0300

    mm

--- index.js
+++ index.js
@@ -5,21 +5,18 @@ const bodyParser = ("body-parser")
 const mongoose = ("mongoose")
 const urlMongoose = "mongodb+srv://lysgut:Mongoatlas177@cluster0.etd9y4p.mongodb.net/${dbname}?retryWrites=true&w=majority"
 const port = 1905
-const ejs = require(ejs)
+const ejs = require("ejs")
 
 app.set("view engine", "ejs")
-app.use(bodyParser.urlencoded({ extended: true}))
-app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-const mongoose = require('mongoose');
 mongoose.Promise = global.Promise; // Utilizar promesas nativas de Node.js
 
 const db = mongoose.connection;
-mongoose.connect('mongodb+srv://lysgut:Mongoatlas177@cluster0.h8ukq9w.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true })
+mongoose.connect(urlMongoose, { useNewUrlParser: true })
   .then(() => console.log('Conexión exitosa a la base de datos'))
   .catch((err) => console.error(err));
 
