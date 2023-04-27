0cdc5dd9152fdbcc4a17950ce2c90861bed46143
lysgut <ltuninetticivarolo@escuelasproa.edu.ar>
2023-04-27 10:23:55 -0300

    uni los npm con index

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require("express")
+const app = express()
+const morgan = require("morgan")
+const bodyParser = ("body-parser")
+const mongoose = ("mongoose")
+const urlMongoose = "mongodb+srv://lysgut:Mongoatlas177@cluster0.etd9y4p.mongodb.net/${dbname}?retryWrites=true&w=majority"
